c293444292b216e509ea25d7e25d355bf1dc2379
asliddin3101 <kamoliddinovasliddin90@gmail.com>
2021-07-31 13:08:24 +0300

    search

--- store/urls.py
+++ store/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path("store/<slug:category_slug>/", category_products , name="category-products-page"),
     path("store/<slug:category_slug>/<slug:sub_category_slug>/", sub_category_products, name="subcategory-products-page"),
     path("product/<slug:slug>/", product_detail,name="product-detail"),
-   
 ]
--- store/views.py
+++ store/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404,render
 from django.urls import reverse
 
 from .models import Category, Product, SubCategory
@@ -18,33 +17,64 @@ def home(request):
 
 
 
+def search(request):
+    word = request.GET.get('q',None)
+    print(word)
+    if word:
+        product=Product.objects.all().filter(title__contains=word)
+        products = Product.objects.all()
+        paginated = get_paginated(request, products, 3)
 
 
-def store(request):
-    products=Product.objects.all()
+        return product
+
+    else:
+        products = Product.objects.all()
+        paginated = get_paginated(request, products, 3)
+
+
+        context = {
+            "products" : paginated["items"],
+            "pages": paginated["pages"]
+        }
+        return render(request, "store.html",context)
+
+
+
+
+
     
+def store(request):
+    products = Product.objects.all()
     paginated = get_paginated(request, products, 3)
-
+    product=search(request)
     context = {
         "products" : paginated["items"],
-        "pages": paginated["pages"]
-    }
-    return render(request, "store.html",context)
+        "pages": paginated["pages"],
+        "word": product
+        }
 
+    return render(request, "store.html",context)
+    
+
+    
 
 def category_products(request,category_slug):
     category = get_object_or_404(Category, slug=category_slug)
     products = Product.objects.filter(sub_category__category=category)
     products=filter_min_max(request,products)
-
+    product=search(request)
     
     paginated = get_paginated(request, products, 3)
 
     context = {
         "products" : paginated["items"],
-        "pages": paginated["pages"]
-    }
+        "pages": paginated["pages"],
+        "word": product
+        }
     return render(request, "store.html",context)
+    
+
 
 
 def sub_category_products(request,category_slug,sub_category_slug):
@@ -54,12 +84,15 @@ def sub_category_products(request,category_slug,sub_category_slug):
     products = Product.objects.filter(sub_category=subcategory)
     products=filter_min_max(request,products)
 
+    product=search(request)
+    
     paginated = get_paginated(request, products, 3)
 
     context = {
         "products" : paginated["items"],
-        "pages": paginated["pages"]
-    }
+        "pages": paginated["pages"],
+        "word": product
+        }
     return render(request, "store.html",context)
 
 
@@ -73,3 +106,4 @@ def product_detail(request,slug):
         "product":product
     }
     return render(request, "product_detail.html", context)
+
