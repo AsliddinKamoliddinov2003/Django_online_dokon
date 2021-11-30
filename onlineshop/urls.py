c47b0b257e40e69e54a7511f598aa6b2530176f3
asliddin3101 <kamoliddinovasliddin90@gmail.com>
2021-11-30 20:27:56 +0300

    k

--- onlineshop/urls.py
+++ onlineshop/urls.py
@@ -10,10 +10,10 @@ urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
     path("account/", include("accounts.urls")),
     path("simpleforms/", include("simpleforms.urls")),
+    path("profile/", include("profileapp.urls")), 
     path("", include("store.urls")),
     path("", include("shopping.urls")),
-    path("profile/", include("profileapp.urls")),
-    
+
  ) + static(settings.STATIC_URL,document_root=settings.STATIC_ROOT) \
     + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
     
